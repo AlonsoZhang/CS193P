caf23ce533a063becd3462b05166b67b061b9fba
Alonso <code@zhangwu.space>
2016-06-29 18:32:29 +0800

    Lecture 4 code

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Alonso Zhang on 16/6/29.
+//  Copyright © 2016年 Alonso Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+}
+
